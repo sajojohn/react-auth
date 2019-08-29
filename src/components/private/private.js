92fbd825f83dac8624fab9d9dd518a9d7f93b34f
sajo.john@millar-bryce.com <sajo.john@millar-bryce.com>
2019-08-29 12:24:15 +0100

    minor refactoring to use async n await instead of promises

--- src/components/private/private.js
+++ src/components/private/private.js
@@ -3,16 +3,28 @@ import React, { Component } from "react";
 class Private extends Component {
   state = { message: "" };
   componentDidMount() {
-    fetch("/private", {
-      headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` },
-    })
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error("Network response was not ok.");
-      })
-      .then((response) => this.setState({ message: response.message }))
-      .catch((error) => this.setState({ message: error.message }));
+    // fetch("/private", {
+    //   headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` },
+    // })
+    //   .then((response) => {
+    //     if (response.ok) return response.json();
+    //     throw new Error("Network response was not ok.");
+    //   })
+    //   .then((response) => this.setState({ message: response.message }))
+    //   .catch((error) => this.setState({ message: error.message }));
+    this.getData();
   }
+
+  // below method calls the same api using async and  await. This is much cleaner than promises
+  getData = async () => {
+    const apiCall = await fetch("/private", { headers: { Authorization: `Bearer ${this.props.auth.getAccessToken()}` } });
+    if (apiCall.ok) {
+      const data = await apiCall.json();
+      this.setState({ message: data.message });
+    } else {
+      this.setState({ message: "Network response was not ok" });
+    }
+  };
   render() {
     const { message } = this.state;
     return <p>{message}</p>;
--- src/components/public/public.js
+++ src/components/public/public.js
@@ -3,14 +3,26 @@ import React, { Component } from "react";
 class Public extends Component {
   state = { message: "" };
   componentDidMount() {
-    fetch("/public")
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error("Network response was not ok.");
-      })
-      .then((response) => this.setState({ message: response.message }))
-      .catch((error) => this.setState({ message: error.message }));
+    // fetch("/public")
+    //   .then((response) => {
+    //     if (response.ok) return response.json();
+    //     throw new Error("Network response was not ok.");
+    //   })
+    //   .then((response) => this.setState({ message: response.message }))
+    //   .catch((error) => this.setState({ message: error.message }));
+    this.fetchData();
   }
+
+  fetchData = async () => {
+    const apiCall = await fetch("/public");
+
+    if (apiCall.ok) {
+      const response = await apiCall.json();
+      this.setState({ message: response.message });
+    } else {
+      this.setState({ message: "Network response was not ok" });
+    }
+  };
   render() {
     const { message } = this.state;
     return <p>{message}</p>;
